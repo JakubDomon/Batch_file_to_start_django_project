0708911960dcc2c0ce5cc64cb965a7db67d5c70d
JakubDomon <jakubdomonn@gmail.com>
2022-12-08 10:42:25 +0100

    Multiple app adding

--- Django/StartDjangoProject.bat
+++ Django/StartDjangoProject.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p PROJ_NAME="Jaka nazwe ma twoj projekt?: "
+set /p PROJ_NAME="Name of your project: "
 django-admin startproject %PROJ_NAME%
 cd %PROJ_NAME%
 python -m venv %PROJ_NAME%_ENV
@@ -7,5 +7,16 @@ mkdir static
 mkdir media
 mkdir templates
 mkdir apps
-
-
+cd apps
+echo Created Django project
+echo -------------------------------
+:loop
+set /p APP_NAME="App name: "
+django-admin startapp %APP_NAME%
+set /p WANT_MORE="Do you want to add more apps: [Y/N]? "
+if "%WANT_MORE%" == "Y" goto :loop
+if not "%WANT_MORE%" == "Y" goto :exit
+:exit
+echo Created apps
+pause
+exit

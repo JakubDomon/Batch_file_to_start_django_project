1e9b1c25b978b54f2ff5b6ea1d85306d928bfe21
JakubDomon <jakubdomonn@gmail.com>
2022-12-08 10:06:04 +0100

    Quick folder fix

--- Django/StartDjangoProject.bat
+++ Django/StartDjangoProject.bat
@@ -2,10 +2,7 @@
 set /p PROJ_NAME="Jaka nazwe ma twoj projekt?: "
 django-admin startproject %PROJ_NAME%
 cd %PROJ_NAME%
-mkdir ENV
-cd ENV
 python -m venv %PROJ_NAME%_ENV
-cd ..
 mkdir static
 mkdir media
 mkdir templates
